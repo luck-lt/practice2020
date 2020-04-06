19a91c34eca5ff241b5853ab4cde49defb6df03f
lt <1040098876@qq.com>
2020-04-06 13:15:53 +0800

    Initial commit

---
+++ imitation-jd-1011/src/main/java/com/lt/ImitationJd1011Application.java
@@ -0,0 +1,13 @@
+package com.lt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImitationJd1011Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImitationJd1011Application.class, args);
+    }
+
+}
